3b902fa6b7704fae658dfe31e20ac7d762586376
Julia Oliveira <julia.oliveira.silva@outlook.com>
2023-10-27 14:23:18 -0300

    inclusão de texto acima do botão

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const switcher = document.querySelector('#switch button');
 const body = document.body;
+const h1 = document.querySelector('h1');
 
 function toggleTheme() {
   body.classList.toggle('light');
-  localStorage.setItem('themeMode', body.classList);
+  const isLightMode = body.classList.contains('light');
+  localStorage.setItem('themeMode', isLightMode ? 'light' : '');
+
+  h1.innerText = isLightMode ? 'Tema Claro' : 'Tema Escuro';
+  h1.style.color = isLightMode ? 'var(--dark)' : 'var(--light)';
 }
 
 switcher.addEventListener('click', toggleTheme);
 
 document.addEventListener('DOMContentLoaded', () => {
-  const savedClass = localStorage.getItem('themeMode');
-  if (savedClass) {
-    body.classList = savedClass;
+  const savedTheme = localStorage.getItem('themeMode');
+  if (savedTheme) {
+    const isLightMode = savedTheme === 'light';
+    body.classList.toggle('light', isLightMode);
+    h1.innerText = isLightMode ? 'Tema Claro' : 'Tema Escuro';
+    h1.style.color = isLightMode ? 'var(--dark)' : 'var(--light)';
   }
 });
